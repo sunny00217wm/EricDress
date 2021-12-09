37b95c3514fabe353bfe51cb113520989c721957
sunny00217wm <sunny00217wm@gmail.com>
2021-12-09 20:49:54 +0800

    =

--- build.sh
+++ build.sh
@@ -7,19 +7,17 @@ build.sh
 _()
 {
 	echo "$ $*"
-	$*
+	eval $*
 }
 
-echo "----       clone       ----"
-_ git clone --depth 1 https://github.com/sunny00217wm/EricDress-core core
-echo "----  move and remove  ----"
-_ rm -fv package.json package-lock.json
-_ mv -fv core/** ./
-_ rm -rfv core
-_ mv -fv config.ts config/config.ts
-_ mv -fv ./*.txt ./config
-echo "----       build       ----"
-_ npm install
-_ npm run build
+echo "----      clone      ----"
+_ "git clone https://github.com/sunny00217wm/EricDress-core core"
+echo "---- move and remove ----"
+_ "rm -fv ./package.json ./package-lock.json"
+_ "mv -fv ./core/** ./"
+_ "rm -rfv ./core"
+_ "mv -fv ./config.ts ./config/config.ts && mv -fv ./*.txt ./config"
+echo "----      build      ----"
+_ "npm install && npm run build"
 echo "---- copy config/*.txt ----"
-_ cp -fv config/*.txt build/config
+_ "cp -fv config/*.txt build/config"
